88dd05f703b78c069d82b72a062fbaacb44072a0
VladimirZubavlenko <vladimirzubavlenko@icloud.com>
2021-03-04 01:10:53 +0300

    [add] login with jwt and localstorage

--- lesson-3/src/components/LoginForm/@slice.ts
+++ lesson-3/src/components/LoginForm/@slice.ts
@@ -1,17 +1,39 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { RootState } from '../../store'
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { fetchData } from './API'
 
 // Define a type for the slice state
-interface LoginFormState {
+export interface Form {
   login: string;
   password: string;
 }
-
+export interface LoginFormState {
+  login: string;
+  password: string;
+  loading: 'idle' | 'pending' | 'succeeded' | 'failed'
+}
+export  interface Response {
+  type: string;
+  message: {
+    token: string;
+  }
+}
 // Define the initial state using that type
 const initialState: LoginFormState = {
   login: '',
-  password: ''
+  password: '',
+  loading: 'idle'
 }
+
+export const loginUser = createAsyncThunk(
+  'login/auth',
+  async (state: Form, thunkAPI) => {
+    const postOptions = {
+      body: JSON.stringify({ username: state.login, password: state.password }),
+      method: 'POST',
+    };
+    const response = await fetchData('/api/login/', postOptions);
+    return await (response.json()) as Response;
+  })
 
 export const loginFormSlice = createSlice({
   name: 'login',
@@ -24,13 +46,24 @@ export const loginFormSlice = createSlice({
     changePassword: (state, action:PayloadAction<string>) => {
       state.password = action.payload
     },
+  },
+  extraReducers: builder => {
+    builder.addCase(loginUser.pending, (state, action) => {
+      state.loading = 'pending'
+    });
+    builder.addCase(loginUser.fulfilled, (state, action) => {
+      state.loading = 'succeeded';
+      state.password = '';
+      state.login = '';
+      localStorage.setItem('token', action.payload.message.token);
+    });
   }
 })
 
 export const { changeLogin, changePassword } = loginFormSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectLogin = (state: RootState) => state.loginForm.login;
-export const selectPassword = (state: RootState) => state.loginForm.password;
+//export const selectLogin = (state: RootState) => state.loginForm.login;
+//export const selectPassword = (state: RootState) => state.loginForm.password;
 
 export default loginFormSlice.reducer
--- lesson-3/src/components/LoginForm/index.tsx
+++ lesson-3/src/components/LoginForm/index.tsx
@@ -2,23 +2,27 @@ import * as React from 'react';
 import { Button, InputGroup } from '@blueprintjs/core';
 import s from './LoginForm.module.scss'
 import { useAppDispatch, useAppSelector } from '../../hooks';
-import { changeLogin, changePassword } from './@slice'
+import { changeLogin, changePassword, loginUser } from './@slice';
 
 const LoginForm: React.FC  = () => {
   const login = useAppSelector(state => state.loginForm.login);
   const password = useAppSelector(state => state.loginForm.password);
+  const status = useAppSelector(state => state.loginForm.loading)
 
   const dispatch = useAppDispatch();
 
   return (
     <div className={s.root}>
+      <div>{status}</div>
       <InputGroup id="username" placeholder="username"
                   value={login}
                   onChange={(event) => dispatch(changeLogin(event.target.value))}/>
       <InputGroup id="password" placeholder="password"
                   value={password}
                   onChange={(event) => dispatch(changePassword(event.target.value))}/>
-      <Button icon="send-to" intent="success" text="Войти"/>
+      <Button icon="send-to" intent="success" text="Войти" onClick={
+        () => dispatch(loginUser({login, password}))
+      }/>
     </div>
   )
 }
